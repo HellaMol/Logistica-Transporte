55b24f6c65075c14d2f6d58c1c4dc841ea68159f
HellaMol <111105393+HellaMol@users.noreply.github.com>
2023-06-01 22:59:54 -0600

    Add files via upload
    Faltan implementar cambios de métodos y atributos en el UML

--- base.h
+++ base.h
@@ -6,17 +6,25 @@ class Base{
     private:
         float lat;
         float lon;
+        Transporte * trans[20]
 
     public:
         Base();
         Base(float la, float lo):lat(la), lon(lo){};
 
+        void mostrar_transportes();
+
         float get_lat();
         float get_lon();
 
-        void set_lat(float );
-        void set_lon(float );
+        void set_lat(float la);
+        void set_lon(float lo);
 };
+
+void Base::mostrar_transportes() {
+  for (int i = 0; i < unidad; i = i + 1)
+    std::cout << trans[i] -> imprime_transportes();
+}
 
 float Base::get_lat(){
     return lat;
@@ -26,7 +34,7 @@ float Base::get_lon(){
     return lon;
 }
 
-void Base::set_lat(float lat){
+void Base::set_lat(float la){
     lat = la;
 }
 
