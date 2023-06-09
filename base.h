a9916afe59ebf05fd81c166aaff38b5b7802e80f
HellaMol <111105393+HellaMol@users.noreply.github.com>
2023-06-08 22:44:27 -0600

    Add files via upload

--- base.h
+++ base.h
@@ -1,43 +1,38 @@
-
+#ifndef BASE_H
+#define BASE_H
+#include"transporte.h"
 #include<iostream>
-#include <string>
+#include<string>
+#include<sstream>
 
 class Base{
     private:
-        float lat;
-        float lon;
-        Transporte * trans[20]
+        Transporte * trans[20];
+        int contador;
 
     public:
-        Base();
-        Base(float la, float lo):lat(la), lon(lo){};
-
+        Base(){}
         void mostrar_transportes();
-
-        float get_lat();
-        float get_lon();
-
-        void set_lat(float la);
-        void set_lon(float lo);
+        void crear_transportes();
 };
 
 void Base::mostrar_transportes() {
-  for (int i = 0; i < unidad; i = i + 1)
-    std::cout << trans[i] -> imprime_transportes();
+    for (int i = 0; i < contador; i ++){
+        std::cout<< trans[i]->imprime_transportes();
+    }
 }
 
-float Base::get_lat(){
-    return lat;
+void Base::crear_transportes(){
+    trans[contador] = new Terrestre("Box Truck", 001, 0, 100, 4600.00, 12, 40, 1);
+    contador++;
+    trans[contador] = new Terrestre("Hazmat Truck", 002, 1, 110, 4600.00, 12, 40, 1);
+    contador++;
+    trans[contador] = new Maritimo("Container Ship", 003, 1, 50, 50000.00, 30, 150, "Despejado", "Alta");
+    contador++;
+    trans[contador] = new Aereo("Boeing 777-200", 004, 0, 50, 18000.00, 6, 30, "Despejado", "Aire");
+    contador++;
+    trans[contador]= new Aereo("Airbus 320", 005, 1, 50, 1500.00, 4, 20, "Despejado", "Tierra");
+    contador++;
 }
 
-float Base::get_lon(){
-    return lon;
-}
-
-void Base::set_lat(float la){
-    lat = la;
-}
-
-void Base::set_lon(float lo){
-    lon = lo;
-}
+#endif
