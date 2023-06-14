52885626b9ced7c5dc47fd7d9caf0b0dc40c1955
HellaMol <111105393+HellaMol@users.noreply.github.com>
2023-06-14 13:36:42 -0600

    Add files via upload

--- mainLogistica.cpp
+++ mainLogistica.cpp
@@ -30,45 +30,58 @@ int main(){
     Base base;
 
     base.crear_transportes();
-    std::cout << "Bienvenidx al programa de Logistica de Transportes"<< std::endl;
-    std::cout << "En este programa podras manejar la losgistica de "<<\
-    "los transportes de la empresa "<< std::endl;
+    std::cout << "Bienvenidx al programa de Logistica "<<\
+    "de Transportes"<< std::endl;
+    std::cout << "En este programa podras manejar "<<\
+    "la losgistica de los transportes de la empresa "<< std::endl;
 
         while (opcion != "7"){
             std::cout << "Selecciona una opcion:"<< std::endl;
-            std::cout << "1. Buscar transporte por capacidad de kg."<< std::endl;
-            std::cout << "2. Buscar transporte por capacidad de pallets."<< std::endl;
-            std::cout << "3. Buscar transporte por capacidad de cajas."<< std::endl;
-            std::cout << "4. Buscar  transporte por velocidad."<< std::endl;
-            std::cout << "5. Mostrar disponibilidad del transporte."<< std::endl;
-            std::cout << "6. Mostrar transportes de la empresa."<< std::endl;
+            std::cout << "1. Buscar transporte por "<<\
+            "capacidad de kg."<< std::endl;
+            std::cout << "2. Buscar transporte por "<<\
+            "capacidad de pallets."<< std::endl;
+            std::cout << "3. Buscar transporte por "<<\
+            "capacidad de cajas."<< std::endl;
+            std::cout << "4. Buscar  transporte por "<<\
+            "velocidad."<< std::endl;
+            std::cout << "5. Mostrar disponibilidad "<<\
+            "del transporte."<< std::endl;
+            std::cout << "6. Mostrar transportes "<<\
+            "de la empresa."<< std::endl;
             std::cout << "7. Salir del programa."<< std::endl;
 
             std::cin >> opcion;
-            while (opcion != "1" && opcion != "2" && opcion != "3" && opcion != "4"\
-            && opcion != "5" && opcion != "6" && opcion != "7"){
+            while (opcion != "1" && opcion != "2"\
+            && opcion != "3" && opcion != "4" && opcion != "5"\
+            && opcion != "6" && opcion != "7"){
 
-                std::cout << "Selecciona una respuesta valida: 1,2,3,4,5,6 o 7"<< std::endl;
+                std::cout << "Selecciona una "<<\
+                "respuesta valida: 1,2,3,4,5,6 o 7"<< std::endl;
 
                 std::cin >> opcion;
             }
             if (opcion == "1"){
-                std::cout << "Buscar transporte por capacidad de kg:"<< std::endl;
+                std::cout << "Buscar transporte por "<<\
+                "capacidad de kg:"<< std::endl;
                 std::cin >> filtro_kg;
                 base.buscar_por(filtro_kg);
             }
             else if (opcion == "2"){
-                std::cout << "Buscar transporte por capacidad de pallets:"<< std::endl;
+                std::cout << "Buscar transporte por "<<\
+                "capacidad de pallets:"<< std::endl;
                 std::cin >> filtro;
                 base.buscar_porcp(filtro);
             }
             else if (opcion == "3"){
-                std::cout << "Buscar transporte por capacidad de cajas:"<< std::endl;
+                std::cout << "Buscar transporte por "<<\
+                "capacidad de cajas:"<< std::endl;
                 std::cin >> filtro;
                 base.buscar_porcc(filtro);
             }
             else if (opcion == "4"){
-                std::cout << "Buscar transporte por velocidad:"<< std::endl;
+                std::cout << "Buscar transporte por "<<\
+                "velocidad:"<< std::endl;
                 std::cin >> filtro;
                 base.buscar_por(filtro);
             }
@@ -81,7 +94,8 @@ int main(){
                 base.mostrar_transportes();
             }
             else if (opcion == "7"){
-                std::cout << "Gracias por usar el programa."<< std::endl;
+                std::cout << "Gracias por usar "<<\
+                "el programa."<< std::endl;
                 return 0;
             }
         }
