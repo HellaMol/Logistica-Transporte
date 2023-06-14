425ec46b712ad6808463925f0eae1601ce5d7c2c
HellaMol <111105393+HellaMol@users.noreply.github.com>
2023-06-14 01:54:23 -0600

    Add files via upload
    Se comentó el código

--- base.h
+++ base.h
@@ -1,38 +1,90 @@
+/*
+ *
+ * Proyecto Logistica de Transportes
+ * Laura Helena Molina Jimenez
+ * A01706282
+ * 14/06/2023
+ * Esta clase define al objeto tipo Base, la cual contiene
+ * los metodos respectivos para mostrar los transportes
+ * con sus respectivos atributos y su disponibilidad.
+ * Asimismo contiene metodos para filtrar por atributo.
+ */
+
 #ifndef BASE_H
 #define BASE_H
-#include"transporte.h"
+
+//Se incluyen bibliotecas para imprimir
 #include<iostream>
 #include<string>
 #include<sstream>
 
+//Se incluye archivo de clase Transporte
+#include "transporte.h"
+
+//Se declara clase Base
 class Base{
+
+    //Se declaran atributos privados de Base
     private:
-        Transporte * trans[20];
+        Transporte * trans[20]; //Se define como apuntador para usar polimorfismo
         int contador;
 
+    //Se declaran metodos de la clase
     public:
-        Base(): contador(0){}
+        Base(): contador(0){} //Se define constructor por default
+        void crear_transportes();
         void mostrar_transportes();
         void mostrar_transdis();
-        void crear_transportes();
         void buscar_por(float cap_kg );
         void buscar_por(int vel_max );
         void buscar_porcp(int cap_pallets );
         void buscar_porcc(int cap_cajas );
 };
 
+/**
+ * mostrar_transportes imprime transportes
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer  el
+ * arreglo imprimiendo cada objeto con sus atributos usando el
+ * metodo imprime_transportes().
+ *
+ * @param
+ * @return
+ */
 void Base::mostrar_transportes(){
     for (int i = 0; i < contador; i = i + 1){
         std::cout<< trans[i]->imprime_transportes();
     }
 }
 
+/**
+ * mostrar_transdis imprime disponibilidad de transportes
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer  el
+ * arreglo imprimiendo el nombre de cada objeto y su disponibilidad
+ * usando el metodo imprime_disponibilidad().
+ *
+ * @param
+ * @return
+ */
 void Base::mostrar_transdis(){
     for (int i = 0; i < contador; i = i + 1){
         std::cout<< trans[i]->imprime_disponibilidad();
     }
 }
 
+/**
+ * buscar_por imprime los transportes que entren en el rango
+ * deseado de cap_kg.
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer el
+ * arreglo imprimiendo cada objeto que tenga una cantidad
+ * superior al float cap_kg.
+ *
+ *
+ * @param float cap_kg
+ * @return
+ */
 void Base::buscar_por(float cap_kg ){
     for (int i = 0; i < contador; i = i + 1){
         if(trans[i]->get_cap_kg() > cap_kg)
@@ -40,6 +92,17 @@ void Base::buscar_por(float cap_kg ){
     }
 }
 
+/**
+ * buscar_porcp imprime los transportes que entren en el rango
+ * deseado de cap_pallets.
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer el
+ * arreglo imprimiendo cada objeto que tenga una cantidad
+ * superior al int cap_pallets
+ *
+ * @param int cap_pallets
+ * @return
+ */
 void Base::buscar_porcp(int cap_pallets ){
     for (int i = 0; i < contador; i = i + 1){
         if(trans[i]->get_cap_pallets() > cap_pallets)
@@ -47,6 +110,18 @@ void Base::buscar_porcp(int cap_pallets ){
     }
 }
 
+/**
+ * buscar_porcc imprime los transportes que entren en el rango
+ * deseado de cap_cajas.
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer el
+ * arreglo imprimiendo cada objeto que tenga una cantidad
+ * superior al int cap_cajas
+ *
+ *
+ * @param int cap_cajas
+ * @return
+ */
 void Base::buscar_porcc(int cap_cajas ){
     for (int i = 0; i < contador; i = i+1){
         if(trans[i]->get_cap_cajas() > cap_cajas)
@@ -54,6 +129,17 @@ void Base::buscar_porcc(int cap_cajas ){
     }
 }
 
+/**
+ * buscar_por imprime los transportes que entren en el rango
+ * deseado de vel_max
+ *
+ * utiliza el arreglo trans[] y el contador, para recorrer el
+ * arreglo imprimiendo cada objeto que tenga una cantidad
+ * superior al int vel_max
+ *
+ * @param int vel_max
+ * @return
+ */
 void Base::buscar_por(int vel_max ){
     for (int i = 0; i < contador; i = i+1){
         if(trans[i]->get_vel_max() > vel_max)
@@ -61,10 +147,20 @@ void Base::buscar_por(int vel_max ){
     }
 }
 
+/**
+ * crea_transportes genera objetos en Transporte[]
+ *
+ * crea objetos tipo Terrestre, Maritimo y Aereo y los
+ * guarda en atributo trans[] (lista de transportes)
+ * para registrar los transportes de la empresa.
+ *
+ * @param
+ * @return
+ */
 void Base::crear_transportes(){
-    trans[contador] = new Terrestre("Box Truck", 001, 0, 100, 4600.00, 12, 40, true);
+    trans[contador] = new Terrestre("Box Truck", 001, 0, 100, 4600.00, 12, 40, 0);
     contador++;
-    trans[contador] = new Terrestre("Hazmat Truck", 002, 1, 110, 4600.00, 12, 40, true);
+    trans[contador] = new Terrestre("Hazmat Truck", 002, 1, 110, 4600.00, 12, 40, 0);
     contador++;
     trans[contador] = new Maritimo("Container Ship", 003, 1, 50, 50000.00, 30, 150, "Despejado", "Alta");
     contador++;
@@ -74,4 +170,4 @@ void Base::crear_transportes(){
     contador++;
 }
 
-#endif
+#endif //BASE_H
